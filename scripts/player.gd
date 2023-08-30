16f39ba790d023bd8530fcd320d7a4a3b40b6b61
troido <troido@protonmail.com>
2023-08-30 12:12:29 +0200

    godot 2 and domain warping

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,10 @@ extends CharacterBody3D
 const MOUSE_SENSITIVITY = 0.003
 const speed = 3
 const sprint_speed = 100
+const gravity = 9.81
+const jump_speed = 10
+
+var gravity_enabled: bool = false
 
 
 
@@ -13,7 +17,12 @@ func _physics_process(delta):
 	var input_movement = Input.get_vector("left", "right", "forwards", "backwards")
 	var s = speed if not Input.is_action_pressed("sprint") else sprint_speed
 	var movement = (Vector3(input_movement.y, 0, -input_movement.x) * s).rotated(Vector3(0, 1, 0), self.rotation.y)
-	movement.y = s * (float(Input.is_action_pressed("up")) - float(Input.is_action_pressed("down")))
+	if gravity_enabled:
+		movement.y = velocity.y - gravity*delta
+		if Input.is_action_pressed("up") and is_on_floor():
+			movement.y = s
+	else:
+		movement.y = s * (float(Input.is_action_pressed("up")) - float(Input.is_action_pressed("down")))
 	velocity = movement
 
 	move_and_slide()
@@ -26,6 +35,8 @@ func _input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if Input.is_action_just_pressed("click"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if Input.is_action_just_pressed("gravity"):
+		gravity_enabled = not gravity_enabled
 
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		# $Head.rotate_z(event.relative.y * MOUSE_SENSITIVITY)
